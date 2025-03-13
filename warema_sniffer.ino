1fb7a1bd93c967ada470273e1347dd81dc092bc9
tombrain <git@meyersonline.de>
2025-03-13 22:44:20 +0100

    Refactor warema_sniffer.ino: Replace std::vector with C-array for data storage, simplify airScan function, and improve code readability

--- warema_sniffer.ino
+++ warema_sniffer.ino
@@ -1,16 +1,17 @@
 #include <ESP8266WiFi.h>
-#include <map>
-#include <string>
 
-const int32_t durationMs_{1000};
-uint8_t gpioPin_{0};
-std::vector<bool> data_;
+const int durationMs_ = 1000;
+const int gpioPin_ = 0;
+const int SAMPLES = (durationMs_ * 1000) / 100;
+bool data_[SAMPLES];  // C-Array statt std::vector
+
 void readCode();
-void airScan(void);
+void airScan();
 
 void setup() {
   Serial.begin(115200);
   Serial.println("Starting continuous scan...");
+  pinMode(gpioPin_, INPUT);
 }
 
 void loop() {
@@ -24,118 +25,94 @@ void loop() {
   }
 }
 
-void airScan(void) {
-    const int32_t MICROSECONDS_PER_MILLISECOND = 1000;
-    const int32_t SAMPLES = (durationMs_ * MICROSECONDS_PER_MILLISECOND) / 100;
-
-    pinMode(gpioPin_, INPUT);
-
-    data_.clear();
-    while (data_.size() < static_cast<size_t>(SAMPLES)) {
-        data_.push_back(digitalRead(gpioPin_) > 0);
-        delayMicroseconds(100);
+void airScan() {
+    for (int i = 0; i < SAMPLES; i++) {
+        data_[i] = GPIO_INPUT_GET(gpioPin_);
+        delayMicroseconds(100);  // evtl. mit Timer oder Interrupt ersetzen
     }
 }
 
 void readCode() {
     struct Waremacode {
-        std::string command{};
-        std::string device1{};
-        std::string device2{};
-        int count{0};
+        String command;
+        String device1;
+        String device2;
+        int count = 0;
 
         void clear() {
-            command.clear();
-            device1.clear();
-            device2.clear();
+            command = "";
+            device1 = "";
+            device2 = "";
             count = 0;
         }
 
-        auto key() const {
-            return command + device1 + device1;
+        String key() const {
+            return command + device1 + device2;
         }
 
         void dump() const {
-            Serial.print("Count: ");
-            Serial.println(count);
-            Serial.println("Code: ");
-            Serial.println(command.c_str());
-            Serial.println(device1.c_str());
-            Serial.println(device2.c_str());
+            Serial.printf("Count: %d\n", count);
+            Serial.println("Code: " + command);
+            Serial.println(device1);
+            Serial.println(device2);
         }
     };
 
-    std::map<std::string, Waremacode> mapKeyCode{};
-    std::vector<Waremacode> collectedCodes{};
-
-    bool previousData{false};
-    int constexpr iLOW{1};
-    int constexpr iHIGH{2};
-    int cntLow{0};
-    int cntHigh{0};
-    int lastCntLow{0};
-    int lastCntHigt{0};
-    int startReading{0};
-    std::string actualBinarycode{};
-    Waremacode actualWaremacode{};
+    const int iLOW = 1;
+    const int iHIGH = 2;
+    int cntLow = 0, cntHigh = 0, lastCntLow = 0, lastCntHigh = 0;
+    bool previousData = false;
+    int startReading = 0;
+    String actualBinarycode;
+    Waremacode actualWaremacode;
     int last = 0;
 
-    for (size_t i{0}; i < data_.size(); i++) {
-        auto const isHigh{data_[i]};
+    for (size_t i = 0; i < SAMPLES; i++) {
+        bool isHigh = data_[i];
 
         if ((isHigh && !previousData) || (!isHigh && previousData)) {
             lastCntLow = cntLow;
-            lastCntHigt = cntHigh;
+            lastCntHigh = cntHigh;
 
             if (last != iLOW) cntLow = 0;
             if (last != iHIGH) cntHigh = 0;
 
-            if (lastCntLow > 50 && lastCntHigt > 30) {
+            if (lastCntLow > 50 && lastCntHigh > 30) {
                 if (last == iLOW) {
                     i++;
                     startReading = 1;
                     cntLow = 0;
                     cntHigh = 0;
                     lastCntLow = 0;
-                    lastCntHigt = 0;
+                    lastCntHigh = 0;
                     previousData = isHigh;
                     continue;
                 }
             }
 
             if (startReading == 1) {
-                if ((lastCntHigt + lastCntLow) >= 10) {
-                    if (lastCntHigt > 25) {
-                        auto const len{actualBinarycode.size()};
-                        if (len == 15) {
+                if ((lastCntHigh + lastCntLow) >= 10) {
+                    if (lastCntHigh > 25) {
+                        if (actualBinarycode.length() == 15) {
                             actualWaremacode.clear();
                             actualWaremacode.command = actualBinarycode;
                             actualWaremacode.count = 1;
-                        } else if (len == 10) {
-                            if (actualWaremacode.device1.empty()) {
+                        } else if (actualBinarycode.length() == 10) {
+                            if (actualWaremacode.device1 == "") {
                                 actualWaremacode.device1 = actualBinarycode;
                             } else {
                                 actualWaremacode.device2 = actualBinarycode;
-                                if (mapKeyCode.find(actualWaremacode.key()) != mapKeyCode.end()) {
-                                    mapKeyCode[actualWaremacode.key()].count++;
-                                } else {
-                                    mapKeyCode.insert({actualWaremacode.key(), actualWaremacode});
-                                }
-                                collectedCodes.push_back(actualWaremacode);
+                                Serial.println("Empfangenes Signal:");
+                                actualWaremacode.dump();
                             }
                         }
                         actualBinarycode = 'S';
                     }
-                    auto const nextLine{data_[i + 1]};
-                    if (!nextLine) {
-                        actualBinarycode += '0';
-                    } else {
-                        actualBinarycode += '1';
-                    }
+                    actualBinarycode += data_[i + 1] ? '1' : '0';
                     cntLow = 0;
                     cntHigh = 0;
                     lastCntLow = 0;
-                    lastCntHigt = 0;
+                    lastCntHigh = 0;
                 }
             }
         }
@@ -148,9 +125,5 @@ void readCode() {
             last = iHIGH;
         }
         previousData = isHigh;
-    }
-
-    for (auto const& e : mapKeyCode) {
-        e.second.dump();
     }
 }
